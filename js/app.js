c683717e9fcb5a7e0134f4b437292e0034d6de77
Aaron Sekisambu <aaron.sekisambu@gmail.com>
2018-12-23 10:04:51 +0300

    Fuctionality on the create view button

--- js/app.js
+++ js/app.js
@@ -2,18 +2,43 @@
 
 
 
-    // Get the button that opens the Meetup window
-    let btnCreateNewMeetup = document.getElementById('btn-new-meetups');
-    // Get the img that closes the meet up
-    let btnCloseNewMeetup = document.getElementById('btn-close');
-    // Get the new meetup window
-    let createNewMeetupWindow = document.getElementById('parent-qstn-area');
+    // Get the button that opens the comment window
+    let btnOpenCommentArea = document.getElementById('on-off-meetup-btns-comment');
+    // Get the img that closes the comment window
+    let btnCloseCommentArea = document.getElementById('btn-close-comment');
+    // Get the comment window
+    let createCommentWindow = document.getElementById('begin-comment-area');
+ 
 
 
-    // Listen for a click
-    btnCreateNewMeetup.addEventListener('click', openWindow);
+    // Listen for open click
+    btnOpenCommentArea.addEventListener('click', openComment);
 
-    //function to open window
-    function openWindow () {
-        console.log(123);
+    // Listen for close click
+    btnCloseCommentArea.addEventListener('click', closeComment);
+
+    //function to open coment area
+    function openComment () {
+        createCommentWindow.style.opacity = 1;
+        console.log('Open')
     }
+
+    // function to close comment area
+    function closeComment () {
+        createCommentWindow.style.opacity = 0;
+        console.log('closed')
+    }
+
+  
+
+    currentDate = () => {
+        let today = new Date();
+        let dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+        return today.toLocaleDateString('en-US', dateOptions)
+    };
+    
+      // Difine todays Month and year.
+      document.getElementById('current-month').innerText = currentDate();
+    
+
+
