fe60f2c2c94ec79e963d277a11e0dd642208cc38
Aaron Sekisambu <aaron.sekisambu@gmail.com>
2019-01-24 22:06:18 +0200

    Testing heroku

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require('./app');
 
-
-const port = parseInt(process.env.PORT, 10) || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening at port ${port}....`));
